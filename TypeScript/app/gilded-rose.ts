16a0b00798635267fb84e132f4d7df856add6c96
raulWTW <raul@wheeltheworld.com>
2022-08-09 13:45:18 -0500

    Rename strings

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -26,18 +26,16 @@ export class GildedRose {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert" &&
-        this.items[i].name != "Sulfuras, Hand of Ragnaros" &&
+        this.items[i].name != Names.BRIE &&
+        this.items[i].name != Names.BACKSTAGE_PASS &&
+        this.items[i].name != Names.SULFURAS &&
         this.items[i].quality > 0
       ) {
         this.items[i].quality = this.items[i].quality - 1; //propiedad generica
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1; //aged brie && backstage pass
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+          if (this.items[i].name == Names.BACKSTAGE_PASS) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1; //backstage pass
@@ -53,12 +51,10 @@ export class GildedRose {
       }
       this.updateSellIn(i);
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+        if (this.items[i].name != Names.BRIE) {
+          if (this.items[i].name != Names.BACKSTAGE_PASS) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              if (this.items[i].name != Names.SULFURAS) {
                 this.items[i].quality = this.items[i].quality - 1; //generica
               }
             }
@@ -78,7 +74,7 @@ export class GildedRose {
   }
 
   private updateSellIn(i: number) {
-    if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+    if (this.items[i].name != Names.SULFURAS) {
       this.items[i].sellIn = this.items[i].sellIn - 1;
     }
   }
